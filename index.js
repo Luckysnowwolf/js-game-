9684e3a73a7d412cb248ab0eba9307e6257e98da
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-11-13 17:27:45 +0100

    changes to index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+// Variables for the DOM elements
+
+// Array
+const words = [
+    "dependent",
+    "dog",
+    "superficial",
+    "admit",
+    "juice",
+    "javascript",
+    "developer",
+    "airplane",
+    "great",
+    "fun",
+    "manipulate",
+    "cat",
+    "transition",
+    "school",
+    "computer",
+    "programming",
+    "drag",
+    "loving",
+    "north",
+  ];
+  const Time = 30 * 1000;
+  function newgame(){
+    
+  document.getElementById(words);
+  if (words === yourtypedword){ 
+    consoel.log("corect")
+  } else (gameover)
+  document.getElementById(text);
+  
+  
+  const difficultylevel = document.getelementbyid(difficulty)
+  if (difficultyleveleasy){
+    console.log("you have picked easy")
+  } else if (difficultylevelmedium){
+    console.log("you have picked medium")
+  } else if(difficultylevelhard){
+    consoel.log("you have picked hard")
+  }
+  document.getelementbyvalue(easy);
+  {}
+  document.getelementbyvalue(medium);
+  {}
+  document.getelementbyvalue(hard);
+  {}
+  document.getelementbyid(time);
+  if (thetimer > 0)
+    console.log()
+  else (thetimer == 0)
+  console.log();
+  document.getelementbyid(score);
+  {}
+  const gameover = consoel.log(`this is your end score "  " ${score}`);
+  }
