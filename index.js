6025c12d1073b71d4a70e90ac5e456fe1d1aa940
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-11-14 11:42:31 +0100

    update to index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-// Variables for the DOM elements
+window.addEventListener(`load`, init);
+input.addeventlistener("input", function (e) {
+    if (e.input === words) {
+        console.log(this.value)
+    }
+});
+let time = 5;
+let score = 0;
+let isplaying;
 
-// Array
+
+
+
+const currentword = document.getElementById(words)
+const wordinput = document.getElementById(text)
+const difficultylevel = document.getelementbyid(difficulty)
+const difficultylevelez = document.getelementbyvalue(easy)
+const difficultylevelmed = document.getelementbyvalue(medium)
+const difficultylevelhard = document.getelementbyvalue(hard)
+const timeDisplay = document.getelementbyid(time)
+const scoreDisplay = document.getelementbyid(score)
 const words = [
     "dependent",
     "dog",
@@ -21,42 +39,69 @@ const words = [
     "drag",
     "loving",
     "north",
-  ];
-  input.addeventlistener("input", function (e) {
-    if (e.input === words){
-        console.log(this.value)
+];
+// initilize game
+function init() {
+    // loads word form array
+    showWord(words);
+    //start match word input
+    wordinput.input.addeventlistener("input", newgame);
+    // call countdown every second
+    setInterval(countdown, 1000)
+    // check game status
+    setInterval(checkstatus, 50);
+}
+// START MATCH
+function newgame() {
+    if (matchword) {
+      isplaying = true;
+      time = 6;
+      showWord(words);
+      wordinput.value = "";
+      score++;
+    } 
+    // if score is -1 display 0
+    if(score === -1){
+        scoreDisplay.innerHTML = 0;
+    } else{
+    scoreDisplay.innerHTML = score;
+}
+}
+//match current word
+function matchword() {
+    if (wordinput.value === currentword.innerHTML) {
+        message.innerHTML = "correct";
+        return true;
+    } else{
+        message.innerHTML = "";
+        return false;
     }
-  });
-  let timer = 30 * 1000;
-  setInterval(newgame, 30 * 1000);
-  function newgame(){
-    console.log(timer++);
-    if( timer === 0){
-        clearInterval(gameover)
+
+}
+// pick and show random word
+function showWord(words) {
+    // generate random array index
+    const randIndex = math.floor(math.random() * words.length);
+    // output random word
+    currentword.innerHtml = words[randIndex];
+}
+function countdown() {
+    // make sure time has not run out 
+    if (time > 0) {
+        //decrement
+        time--;
+    } else if (time === 0) {
+        // game is over
+        isplaying = false;
     }
-  document.getElementById(words).addeventlistener(){};
-  document.getElementById(text).addeventlistener(){};
-  
-  
-  const difficultylevel = document.getelementbyid(difficulty).addeventlistener(){};
-  if (difficultyleveleasy){
-    console.log("you have picked easy")
-  } else if (difficultylevelmedium){
-    console.log("you have picked medium")
-  } else if(difficultylevelhard){
-    consoel.log("you have picked hard")
-  }
-  document.getelementbyvalue(easy).addeventlistener(){};
-  {}
-  document.getelementbyvalue(medium).addeventlistener(){};
-  {}
-  document.getelementbyvalue(hard).addeventlistener(){};
-  {}
-  document.getelementbyid(time).addeventlistener(){};
-  if (timer > 0)
-    console.log()
-  else (timer == 0)
-  console.log();
-  document.getelementbyid(score).addeventlistener(){};
-  const gameover = consoel.log(`this is your end score "  " ${score}`);
-  }
+    // show time
+    timeDisplay.innerHTML = time;
+}
+
+//Check game status
+function checkstatus() {
+    if (isplaying && time === 0) {
+        message.innerHTML = "Game Over";
+        score = -1;
+    }
+}
